9dde2880e03fae1aded8e3c0409a597f46d7ec12
Pedro <necromancerdoidao@outlook.com>
2022-11-24 20:43:06 -0300

    Instalada e implementada lib pytest-mock
    close #35

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -6,22 +6,22 @@ from libpythonpro import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/2248?v=4'
     resp_mock.json.return_value = {
         'login': 'pedro', 'id': 2248,
         'avatar_url': url
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('Pedro')
     assert avatar_url == url
+
 
 def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('PedroH31')
