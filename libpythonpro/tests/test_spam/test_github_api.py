12e8b0ad3d022bafe6505ea1931e31394b1a82c1
Pedro <necromancerdoidao@outlook.com>
2022-11-24 20:27:54 -0300

    isolados imports com o pytest fixture
    close #34

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,13 +1,28 @@
 from unittest.mock import Mock
+
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/2248?v=4'
     resp_mock.json.return_value = {
         'login': 'pedro', 'id': 2248,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/2248?v=4'
+        'avatar_url': url
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('Pedro')
-    assert 'https://avatars.githubusercontent.com/u/2248?v=4' == url
+    assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('PedroH31')
+    assert 'https://avatars.githubusercontent.com/u/95108502?v=4' == url
